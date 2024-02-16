b539962ae7eb0e71ad8d568f63304b470580393a
MatejBursik <r0929146@student.thomasmore.be>
2024-02-16 21:24:31 +0100

    v1 upload

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Animation-tool
+# Animation compiling tool
+
+A command line program which is going to compile your frames of an animation into an video. Because it is an Python program, the command  makes its current working directory where the act.py file is located.
+
+## Prerequisite
+
+- frames of the animation should be numerically labled from 1
+- install python [(link)](https://www.python.org/downloads/)
+- dependency cv2 (`pip install cv2`)
+
+## Arguments
+
+List of arguments available for the command
+
+#### Mandatory
+
+| Argument | Description | Example |
+| -------- | ------- | ------- |
+| `--name` | name of the output video file | `python act.py --name example` |
+
+#### Optional
+
+| Argument | Description | Example | Default |
+| -------- | ------- | ------- | ------- |
+| `--fps` | set fps (frames per second) | `python act.py --name example --fps 25` | 30 |
+| `--img` | specify the source image file type (png, jpeg, tiff) | `python act.py --name example --img jpeg` | png |
+| `--path` | specify the directory of the images | `python act.py --name example --path images` | current working directory |
+
+## Test
+
+In this repository, I provide a set of 45 images to test if it is running correctly. To test it, run command `python act.py --name test --path test` in the cloned repository. It should generate a test.mp4 animation of a dot moving around.
+
+## Future updates
+
+- more output file types (mp4, mov, avi, webm, flv, mkv ...)
---
+++ act.py
@@ -0,0 +1,65 @@
+import cv2, os, sys
+
+def convert(name, fps, imgT, path):
+    if not name:
+        return "You did not input --name argument"
+
+    if not fps:
+        fps = 30
+    elif fps.isdigit() and int(fps) > 0:
+        fps = int(fps)
+    else:
+        return "Your --fps argument was not a positive integer"
+    
+    if not imgT:
+        imgT = "png"
+    elif imgT not in ["png", "jpeg", "tiff"]:
+        return "Your --img argument is not supported"
+    
+    if not path:
+        files = list(filter(lambda x:x[:-len(imgT)-1].isdigit(), os.listdir()))
+        images =  [str(i) + "." + imgT for i in range(1, len(files)+1)]
+    elif not os.path.isdir(path):
+        return "Your --path argument does not exist"
+    else:
+        files = os.listdir(path)
+        images = [path + "\\" + str(i) + "." + imgT for i in range(1, len(files)+1)]
+
+    out_path = name + ".mp4"
+
+    try:
+        frame = cv2.imread(images[0])
+        dimensions = (frame.shape[1],frame.shape[0])
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+        video = cv2.VideoWriter(out_path, fourcc, fps, dimensions)
+
+        for i,img in enumerate(images):
+            video.write(cv2.imread(img))
+            print(f"({i+1}/{len(images)})")
+
+        video.release()
+    except:
+        return "You are missing image/s"
+    
+    return "Finished"
+
+arg = sys.argv
+
+name = False
+fps = False
+path = False
+imgType = False
+
+if "--name" in arg:
+    name = arg[arg.index("--name") + 1]
+
+if "--fps" in arg:
+    fps = arg[arg.index("--fps") + 1]
+
+if "--img" in arg:
+    imgType = arg[arg.index("--img") + 1]
+
+if "--path" in arg:
+    path = arg[arg.index("--path") + 1]
+
+print(convert(name, fps, imgType, path))
